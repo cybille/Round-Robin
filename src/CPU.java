672819bdaa89ba6fc47fb59065946c4d666b55ca
cybille <rebeccacybille@gmail.com>
2021-11-14 20:04:22 -0500

    Changes by jhose

--- src/CPU.java
+++ src/CPU.java
@@ -6,7 +6,11 @@ public class CPU {
     private int turnAroundTime;
     private int waitingTime;
     private int responseTime;
+    private int arrivalTime;
+    private int cpuArrivalTime;
+    private int burstTime;
 
+    ArrivalTime totalTime= new ArrivalTime();
     /*Turnaround time: Completion time - Arrival time
     Waiting time: Turnaround time – Burst time
     Response time: the time at which a process gets cpu first time – arrival time
@@ -14,13 +18,16 @@ public class CPU {
 
     //execute
 
-    public void setTurnAroundTime(int completionTime, int arrivalTime){
-        this.turnAroundTime= completionTime- arrivalTime;
+    public void setTurnAroundTime(int completionTime, int cpuArrivalTime){
+        this.turnAroundTime= completionTime- cpuArrivalTime;
     }
     public void setWaitingTime(int turnAroundTime, int burstTime){
+
         this.waitingTime= turnAroundTime- burstTime;
     }
-    public void setResponseTime(int response ){}
+    public void setResponseTime(int idleTime, int cpuArrivalTime){
+        this.responseTime= idleTime- cpuArrivalTime;
+    }
 
 
 
